d44ae92e5361053a80ee196d9148bdb5016f5060
Jaroslav Zaba <jaroslav.zaba@post.cz>
2024-04-12 17:10:58 +0200

    dbHandler done

--- dbHandler.py
+++ dbHandler.py
@@ -35,35 +35,109 @@ def addServer(name: str, roomcode: str):
     # Table for users
     cursor.execute('INSERT INTO servers(name, code) VALUES(?, ?);', (name, roomcode))
   except sqlite3.Error as e:
-    logger.log('An error in SQL syntax occurred while initializing tables')
+    logger.log(f'An error in SQL syntax occurred while adding server; Error message: {e}')
   except Exception as e:
-    logger.log(f'An unexpected error occurred while initializing tables; Error message: {e}')
+    logger.log(f'An unexpected error occurred while adding server; Error message: {e}')
   db.commit()
   return True
 
 
 def checkIfCodeExists(roomcode: str):
-  pass
-
-
-def checkIfServerExists(roomcode: str):
-  pass
+  try:
+    db = sqlite3.connect('data/database.db')
+    cursor = db.cursor()
+    # Table for users
+    res = cursor.execute('SELECT * FROM servers WHERE code = ?', (roomcode,))
+    res = res.fetchall()
+    db.commit()
+    if res:
+      return True
+    return False
+  except sqlite3.Error as e:
+    logger.log(f'An error in SQL syntax occurred while checking if code exists; Error message: {e}')
+  except Exception as e:
+    logger.log(f'An unexpected error occurred while checking if code exists; Error message: {e}')
+  db.commit()
+  return True
 
 
 def removeServer(roomcode: str):
-  pass
+  try:
+    db = sqlite3.connect('data/database.db')
+    cursor = db.cursor()
+    # Find a user
+    srv = cursor.execute('SELECT * FROM servers WHERE code = ?;', (roomcode,))
+    srv = srv.fetchone()
+    if srv:
+      cursor.execute('DELETE FROM servers WHERE code = ?', (roomcode,))
+    else:
+      # If we were unable to find the code in servers, we log it as a warning
+      logger.log(f'Recieved remove request for code({roomcode}), however requested row is not present in servers table aborting', 2)
+  except sqlite3.Error as e:
+    logger.log(f'An error in SQL syntax occurred while removing a server; Error message: {e}; Data: {(roomcode)}')
+  except Exception as e:
+    logger.log(f'An unexpected error occurred while removing a user; Error message: {e}')
+  db.commit()
+  return True
 
 
 def getServerId(roomcode: str):
-  pass
+  try:
+    db = sqlite3.connect('data/database.db')
+    cursor = db.cursor()
+    # Table for users
+    res = cursor.execute('SELECT id FROM servers WHERE code = ?', (roomcode,))
+    res = res.fetchone()
+    db.commit()
+    if res:
+      return res[0]
+    logger.log(f'Server id for {roomcode} not present!')
+    return -1
+  except sqlite3.Error as e:
+    logger.log(f'An error in SQL syntax occurred while getting serverId; Error message: {e}')
+  except Exception as e:
+    logger.log(f'An unexpected error occurred while getting serverId; Error message: {e}')
+  db.commit()
+  return -1
 
 
 def addMessage(roomcode: str, message: str):
-  pass
+  try:
+    if (not checkIfCodeExists(roomcode)):
+      logger.log(f'Roomcode {roomcode} not present!', 2)
+      return False
+    db = sqlite3.connect('data/database.db')
+    cursor = db.cursor()
+    ix = getServerId(roomcode)
+    cursor.execute('INSERT INTO messages(message, serverId) VALUES(?, ?);', (message, ix))
+  except sqlite3.Error as e:
+    logger.log(f'An error in SQL syntax occurred while adding message; Error message: {e}')
+  except Exception as e:
+    logger.log(f'An unexpected error occurred while adding message; Error message: {e}')
+  db.commit()
+  return True
 
 
 def getMessages(roomcode: str):
-  pass
+  try:
+    if (not checkIfCodeExists(roomcode)):
+      logger.log(f'Roomcode {roomcode} not present!', 2)
+      return False
+    db = sqlite3.connect('data/database.db')
+    cursor = db.cursor()
+    ix = getServerId(roomcode)
+    res = cursor.execute('SELECT message FROM messages WHERE serverId = ? ORDER BY message ASC;', (ix,))
+    res = res.fetchall()
+    db.commit()
+    if res:
+      return res
+    return []
+  except sqlite3.Error as e:
+    logger.log(f'An error in SQL syntax occurred while getting messages; Error message: {e}')
+  except Exception as e:
+    logger.log(f'An unexpected error occurred while getting messages; Error message: {e}')
+  db.commit()
+  return []
 
 #REMOVE FROM FINAL VERSION
 def selectall(): # Used for displaying data REMOVE FROM FINAL VERSION
