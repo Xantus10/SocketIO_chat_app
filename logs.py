b933f4b284e7655816e4727b6c3339c1fd470113
Jaroslav Zaba <jaroslav.zaba@post.cz>
2024-08-08 21:54:57 +0200

    Forgot to add updated logs.py

--- logs.py
+++ logs.py
@@ -1,22 +1,32 @@
 from datetime import datetime
 
-# 1-3 for Message (*), Warning (#), Error (!)
-def log(msg: str, type=3):
-  try:
-    # Identificators for messages ([*], [!], etc.)
-    if type == 1:
-      descriptionChar = '*'
-    elif type == 2:
-      descriptionChar = '#'
-    elif type == 3:
-      descriptionChar = '!'
-    else:
-      # Use [U] for Unidentified logs
-      descriptionChar = 'U'
-    # Format the log Into: [ID] (date time) - message with newlines replaced with ;;
-    toLog = f'[{descriptionChar}] ({datetime.now().strftime("%d.%m.%Y %H:%M:%S")}) - {msg.replace(chr(10), ";;")}{chr(10)}'
-    # Write to logfile
-    with open('data/log.txt', 'a') as f:
-      f.write(toLog)
-  except Exception as e:
-    print(f'ERROR Unexpected Exception occurred while logging: {e}')
+
+class Logger:
+  def __init__(self):
+    self.CONFIG = {}
+    self.CONFIG['OUTPUT_FILE'] = 'data/log.txt'
+    # Default: 0 - all, otherwise the log level should be 1 lower than priority passed to log() - to log only Error(3) and above LOG_LEVEL should be 2
+    self.CONFIG['LOG_LEVEL'] = 0
+
+  # 1-3 for Message (*), Warning (#), Error (!)
+  def log(msg: str, ptype=3):
+    try:
+      if ptype < self.CONFIG['LOG_LEVEL']: return
+      # Identificators for messages ([*], [!], etc.)
+      if ptype == 1:
+        descriptionChar = '*'
+      elif ptype == 2:
+        descriptionChar = '#'
+      elif ptype == 3:
+        descriptionChar = '!'
+      else:
+        # Use [U] for Unidentified logs
+        descriptionChar = 'U'
+      # Format the log Into: [ID] (date time) - message with newlines replaced with ;;
+      toLog = f'[{descriptionChar}] ({datetime.now().strftime("%d.%m.%Y %H:%M:%S")}) - {msg.replace(chr(10), ";;")}{chr(10)}'
+      # Write to logfile
+      with open('data/log.txt', 'a') as f:
+        f.write(toLog)
+        f.close()
+    except Exception as e:
+      print(f'ERROR Unexpected Exception occurred while logging: {e}\nPLEASE RESOLVE IMMEDIATLY')
